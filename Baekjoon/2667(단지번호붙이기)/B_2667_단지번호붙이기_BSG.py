cd9ba8e7187ad4c8cc647c5429519e5cb57976ee
byeonsangguk <47583160+byeonsangguk@users.noreply.github.com>
2019-07-31 10:47:31 +0900

    Add files via upload

---
+++ Baekjoon/2667(단지번호붙이기)/B_2667_단지번호붙이기_BSG.py
@@ -0,0 +1,38 @@
+import sys
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+count=0
+danji=[]
+N = int(sys.stdin.readline())
+for i in range(N):
+    matrix=[list(sys.stdin.readline())]
+
+def search(x, y):
+    global count
+    matrix[x][y] = '0'
+    count += 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or nx >= N or ny < 0 or ny >= N:
+            continue
+        if matrix[nx][ny] == '1':
+            search(nx, ny)
+
+
+for i in range(N):
+    for j in range(N):
+        if matrix[i][j] == '1':
+            count = 0
+            search(i, j)
+            danji.append(count)
+
+print(len(danji))
+danji.sort()
+for i in danji:
+    print(i)
+
+
+
+

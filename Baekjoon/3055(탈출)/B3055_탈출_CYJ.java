8da9071bf61b097104565fe0f2a8ea93f73beb55
Yejin <chldPwlsyy@gmail.com>
2019-09-04 12:53:36 +0900

    Update B3055_탈출_CYJ.java

--- Baekjoon/3055(탈출)/B3055_탈출_CYJ.java
+++ Baekjoon/3055(탈출)/B3055_탈출_CYJ.java
@@ -1,152 +1,3 @@
-/*
-//14500 : 테트로미노 
-import java.util.*;
-public class Main{
-	public static void main(String args[]) {
-		int n,m;
-		
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		m = sc.nextInt();
-		
-		int[][] arr = new int[n][m];
-	
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<m;j++){
-				arr[i][j]=sc.nextInt();
-			}
-		}
-		int max = 0;
-		
-		for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-            	
-            	//1x4
-            	if (j+3 < m) {
-            		int temp = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i][j+3];
-                    if (max < temp) max = temp;
-            	}
-            	 
-            	//4x1
-            	if (i+3 < n) {
-            		int temp = arr[i][j] + arr[i+1][j] + arr[i+2][j] + arr[i+3][j];
-                    if (max < temp) max = temp;
-            	}
-            	
-            	//2x2
-            	if (i+1 < n && j+1 < m) {
-            		int temp = arr[i][j] + arr[i+1][j] + arr[i][j+1] + arr[i+1][j+1];
-                    if (max < temp) max = temp;
-            	}
-            	
-            	//2x3
-            	if (i+1 < n && j+2 < m) {
-            	   //4
-            	   int temp = arr[i][j] + arr[i][j+1] 
-            			    + arr[i+1][j+1] + arr[i+1][j+2];
-                   if (max < temp) max = temp;
-                    
-                   //6
-                   temp = arr[i][j+1] + arr[i][j+2] 
-                    	+ arr[i+1][j] + arr[i+1][j+1] ;
-                   if (max < temp) max = temp;
-                    
-                   //8
-                   temp = arr[i][j] + arr[i][j+1] + arr[i][j+2] 
-                   	    + arr[i+1][j+2];
-                   if (max < temp) max = temp;
-                    
-                  //10
-                    temp = arr[i][j]
-                    	 + arr[i+1][j] + arr[i+1][j+1] + arr[i+1][j+2];
-                    if (max < temp) max = temp;
-                    
-                  //14
-                    temp = arr[i][j+2] 
-                    	 + arr[i+1][j] + arr[i+1][j+1] + arr[i+1][j+2];
-                    if (max < temp) max = temp;
-                    
-                  //16
-                    temp = arr[i][j] + arr[i][j+1] + arr[i][j+2] 
-                    	 + arr[i+1][j];
-                    if (max < temp) max = temp;
-                    
-                  //18
-                    temp = arr[i][j] + arr[i][j+1] + arr[i][j+2] 
-                    	 + arr[i+1][j+1];
-                    if (max < temp) max = temp;
-                    
-                  //19
-                    temp = arr[i][j+1]
-                    	 + arr[i+1][j] + arr[i+1][j+1] + arr[i+1][j+2];
-                    if (max < temp) max = temp;
-                    
-            	}
-            	
-            	//3x2
-            	if (i+2 < n && j+1 < m) {
-            		//5
-             	    int temp = arr[i][j+1]
-             			     + arr[i+1][j] + arr[i+1][j+1]
-             			     + arr[i+2][j];
-                    if (max < temp) max = temp;
-                    
-                  //7
-             	    temp = arr[i][j]
-             			 + arr[i+1][j] + arr[i+1][j+1]
-             			 + arr[i+2][j+1];
-                    if (max < temp) max = temp;
-                    
-                  //9
-             	    temp = arr[i][j+1]
-             			 + arr[i+1][j+1]
-             			 + arr[i+2][j] + arr[i+2][j+1];
-                    if (max < temp) max = temp;
-                    
-                  //11
-             	    temp = arr[i][j] + arr[i][j+1]
-             			 + arr[i+1][j] 
-             			 + arr[i+2][j];
-                    if (max < temp) max = temp;
-                    
-                  //12
-             	    temp = arr[i][j]
-             			     + arr[i+1][j] + arr[i+1][j+1]
-             			     + arr[i+2][j];
-                    if (max < temp) max = temp;
-                    
-                  //13
-             	    temp =arr[i][j+1]
-             			 + arr[i+1][j] + arr[i+1][j+1]
-             			 + arr[i+2][j+1];
-                    if (max < temp) max = temp;
-                    
-                  //15
-             	    temp = arr[i][j] + arr[i][j+1]
-             			 + arr[i+1][j+1]
-             			 + arr[i+2][j+1];
-                    if (max < temp) max = temp;
-                    
-                  //17
-             	    temp = arr[i][j]
-             			 + arr[i+1][j]
-             			 + arr[i+2][j] + arr[i+2][j+1];
-                    if (max < temp) max = temp;
-                     
-            	}
-            
-            }
-		}
-		
-        System.out.println(max);	
-		
-	}
-}
-*/
-
-
-
-//3055 : 탈출 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -154,8 +5,8 @@ import java.util.*;
 
 public class Main{
 	
-	static int[] dx = {0,1,0,-1};
-    static int[] dy = {-1,0,1,0};
+	static int dx[] = new int [] {-1,1,0,0};
+	static int dy [] = new int [] {0,0,1,-1};
     static int r;
     static int c;
     
@@ -176,17 +27,19 @@ public class Main{
     
     public static int bfs(char[][] map) {
     	int cnt = 0;
+    	int size = 0;
     	while(!dochi.isEmpty()) {
     		cnt++;
-    		
+    		size = water.size();
     		//water
-    		for(int i=0;i<water.size();i++) {
+    		for(int i=0;i<size;i++) {
     			pair w_xy = water.poll();
 
 		    		for(int j=0;j<4;j++){
 		                int xx = w_xy.x+dx[j];
 		                int yy = w_xy.y+dy[j];
-		                if(xx<0||yy<0||xx>=c||yy>=r){
+		                
+		                if(xx<0||yy<0||xx>=r||yy>=c){
 		                    continue;
 		                }
 		                
@@ -198,18 +51,21 @@ public class Main{
 
     		}
     		
+    		size = dochi.size();
+    		
     		//dochi
-    		for(int i=0;i<dochi.size();i++) {
+    		for(int i=0;i<size;i++) {
     			pair d_xy = dochi.poll();
 
 		    		for(int j=0;j<4;j++){
 		                int xx = d_xy.x+dx[j];
 		                int yy = d_xy.y+dy[j];
-		                if(xx<0||yy<0||xx>=c||yy>=r){
+		                if(xx<0||yy<0||xx>=r||yy>=c){
 		                    continue;
 		                }
-	                
-		                if(!check[xx][yy]&& map[xx][yy]!='*' &&map[xx][yy]!='X') {
+		                
+		                if(!check[xx][yy]&& map[xx][yy]!='*' && map[xx][yy]!='X') {
+		                	
 		                	dochi.add(new pair(xx,yy));
 	
 	                        check[xx][yy] = true;
@@ -234,14 +90,16 @@ public class Main{
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
         
-        map = new char [r][c];
-        check = new boolean[r][c];
+        map = new char[r+1][c+1];
+        check = new boolean[r+1][c+1];
         
        
         for (int i = 0; i < r; i++) {
-            String s = br.readLine().trim();
+        	char input[] = br.readLine().toCharArray();
             for (int j = 0; j < c; j++) {
                 
+            	map[i][j] = input[j];
+            	
                 if(map[i][j] == '*') { 
                     water.add(new pair(i,j));
                 }
@@ -249,16 +107,14 @@ public class Main{
                 	dochi.add(new pair(i,j));
                 	check[i][j] =true;
                 }
-
-                map[i][j] = s.charAt(j);
+                
             }
         }
         
         	
         int result = bfs(map);
         
-		if(result < 0) System.out.print("KAKTUS");
-        if(result > 0) System.out.print(result);
+		if(result == -1) System.out.print("KAKTUS");
+		else System.out.print(result);
 	}
 }
-
